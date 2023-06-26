b475929e35f81d7374b03f903f9c13558f4d5420
Rubendvb <rubenvasquez556@gmail.com>
2023-06-26 01:43:12 -0300

    adicionando imagens no banner

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,6 +1,12 @@
-import Carousel1 from "../assets/images/Mobile/Banner carousel 1 _ 375.png";
-import Carousel2 from "../assets/images/Mobile/Banner carousel 2 _ 375.png";
-import Carousel3 from "../assets/images/Mobile/Banner carousel 3 _ 375.png";
+import Carouselmobile1 from "../assets/images/Mobile/Banner carousel 1 _ 375.png";
+import Carouselmobile2 from "../assets/images/Mobile/Banner carousel 2 _ 375.png";
+import Carouselmobile3 from "../assets/images/Mobile/Banner carousel 3 _ 375.png";
+import Carouseltablet1 from "../assets/images/Tablet/Banner carousel 1 _ 768.png";
+import Carouseltablet2 from "../assets/images/Tablet/Banner carousel 2 _ 768.png";
+import Carouseltablet3 from "../assets/images/Tablet/Banner carousel 3 _ 768.png";
+import Carouseldesktop1 from "../assets/images/Desktop/Banner carousel 1 _ 1440 (1).png";
+import Carouseldesktop2 from "../assets/images/Desktop/Banner carousel 2 _ 1440 (1).png";
+import Carouseldesktop3 from "../assets/images/Desktop/Banner carousel 3 _ 1440 (1).png";
 
 import "../assets/styles/scss/Banner.scss";
 
@@ -31,21 +37,63 @@ export default function Banner() {
       </div>
       <div className="carousel-inner">
         <div className="carousel-item active">
-          <img src={Carousel1} className="d-block w-100" alt="Banner 1" />
+          <img
+            src={Carouselmobile1}
+            className="d-block w-100 banner__img-mobile"
+            alt="Banner 1"
+          />
+          <img
+            src={Carouseltablet1}
+            className="d-block w-100 banner__img-tablet"
+            alt="Banner 1"
+          />
+          <img
+            src={Carouseldesktop1}
+            className="d-block w-100 banner__img-desktop"
+            alt="Banner 1"
+          />
         </div>
         <div className="carousel-item banner__img">
           <div>
             <h3>COLEÇÃO ATEMPORAL</h3>
             <span>Estilo e qualidade para durar. </span>
           </div>
-          <img src={Carousel2} className="d-block w-100" alt="Banner 2" />
+          <img
+            src={Carouselmobile2}
+            className="d-block w-100 banner__img-mobile"
+            alt="Banner 2"
+          />
+          <img
+            src={Carouseltablet2}
+            className="d-block w-100 banner__img-tablet"
+            alt="Banner 2"
+          />
+          <img
+            src={Carouseldesktop2}
+            className="d-block w-100 banner__img-desktop"
+            alt="Banner 2"
+          />
         </div>
         <div className="carousel-item banner__img">
           <div>
             <h3>COLEÇÃO ATEMPORAL</h3>
             <span>Alto impacto visual, baixo impacto ambiental.</span>
           </div>
-          <img src={Carousel3} className="d-block w-100" alt="Banner 3" />
+          <img
+            src={Carouselmobile3}
+            className="d-block w-100 banner__img-mobile"
+            alt="Banner 3"
+          />
+          <img
+            src={Carouseltablet3}
+            className="d-block w-100 banner__img-tablet"
+            alt="Banner 3"
+          />
+          <img
+            src={Carouseldesktop3}
+            className="d-block w-100 banner__img-desktop"
+            alt="Banner 3"
+          />
         </div>
       </div>
       <button
